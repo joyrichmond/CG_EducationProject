c68c9a9223893061a17c754865533e54f8e4b76f
Joy <sjoy.richmond@gmail.com>
2018-12-07 16:10:39 -0600

    initial commit

---
+++ cg_education_project_files/main.js
@@ -0,0 +1,1 @@
+console.log("I hope my code works!")
---
+++ main.js
@@ -0,0 +1,5 @@
+let teacherName = "Mr. Talks-a-lot";
+let department = "Communication";
+let rating = 2.1;
+
+console.log(`Teacher: ${teacherName}\nDepartment: ${department}\nRating: ${rating}`);
