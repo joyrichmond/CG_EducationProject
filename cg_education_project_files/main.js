890003f0b17cd0a8f0d0d73e8c421a2e96397e74
Joy <sjoy.richmond@gmail.com>
2018-12-07 16:19:04 -0600

    challenge 2

--- cg_education_project_files/main.js
+++ cg_education_project_files/main.js
@@ -1,1 +1,7 @@
-console.log("I hope my code works!")
+let teacherName = "Mr. Talks-a-lot";
+let department = "Communication";
+let rating = [2.1, 3.7, 4.3];
+let ratingOutput = rating.join(", ");
+
+console.log(`Teacher: ${teacherName}\nDepartment: ${department}\nRating: ${ratingOutput}`);
+
--- main.js
+++
@@ -1,5 +0,0 @@
-let teacherName = "Mr. Talks-a-lot";
-let department = "Communication";
-let rating = 2.1;
-
-console.log(`Teacher: ${teacherName}\nDepartment: ${department}\nRating: ${rating}`);
