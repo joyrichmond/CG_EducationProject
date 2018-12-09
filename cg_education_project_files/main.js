1df378804e5205bb68aa072c07b83e468616fcd1
Joy <sjoy.richmond@gmail.com>
2018-12-08 19:41:20 -0600

    challenge 4

--- cg_education_project_files/main.js
+++ cg_education_project_files/main.js
@@ -1,4 +1,4 @@
-let teacherName = "Mr. Talks-a-lot";
+let teacherName = "Thomas Talks-a-lot";
 let teacherDepartment = "Communication";
 
 
@@ -13,3 +13,18 @@ let teacherAvgRating = (totalRating / teacherRatings.length).toFixed(5);
 let teacherRatingOutput = teacherRatings.join(", ");
 console.log(`Teacher: ${teacherName}\nDepartment: ${teacherDepartment}\nRating: ${teacherRatingOutput}\nAverage Rating: ${teacherAvgRating}`);
 
+
+let studentName = "Ida Dream";
+let studentMajor = "Communication";
+let studentEmail = "talkinfunny@gmail.com";
+let studentGPA = 3.4;
+
+console.log(`Student Name: ${studentName}\nMajor: ${studentMajor}\nEmail: ${studentEmail}\nGPA: ${studentGPA}`);
+
+
+let courseName = "Interpersonal Communication";
+let courseDepartment = "Communication";
+let courseTeacher = "Thomas Talks-a-lot";
+let courseSemester = "Fall 2019";
+
+console.log(`Course: ${courseName}\nDepartment: ${courseDepartment}\nInstructor: ${courseTeacher}\nSemester: ${courseSemester}`);
