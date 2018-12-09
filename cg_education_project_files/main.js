5cbf088b17020be15a694e0749ba580eee9378dc
Joy <sjoy.richmond@gmail.com>
2018-12-09 16:30:27 -0600

    lesson 9.3 challenge 2

--- cg_education_project_files/main.js
+++ cg_education_project_files/main.js
@@ -1,10 +1,10 @@
 //teacher info variables
-let teacherName = "Thomas Talks-a-lot";
-let teacherDepartment = "Communication";
+const teacherName = "Thomas Talks-a-lot";
+const teacherDepartment = "Communication";
 let teacherRatings = [2.1, 3.7, 4.3];
 
 //function to add value to an array
-let addTeacherRating = (array, value) => {
+const addTeacherRating = (array, value) => {
     array.push(value);
     return array;
 }
@@ -18,21 +18,20 @@ while (true) {
     if ((newRating >= 0) && (newRating <= 5)) {        
         newTeacherRatings = addTeacherRating(teacherRatings, newRating);
         break;
-    } else {
-        alert("You didn't follow directions. Shame on you. Please rate your teacher with a value between 0.0 and 5.0.");
     }
+    alert("You didn't follow directions. Shame on you. Please rate your teacher with a value between 0.0 and 5.0.");
 }
 
 //totals ratings in array
-let totalRating = newTeacherRatings.reduce((carryOver, indexValue) => {
+const totalRating = newTeacherRatings.reduce((carryOver, indexValue) => {
     return carryOver + indexValue;
 });
 
 //averages teacher's ratings
-let teacherAvgRating = (totalRating / newTeacherRatings.length).toFixed(5);
+const teacherAvgRating = (totalRating / newTeacherRatings.length).toFixed(5);
 
 //gives array with spaces between ratings
-let teacherRatingOutput = newTeacherRatings.join(", ");
+const teacherRatingOutput = newTeacherRatings.join(", ");
 
 //alerts student to new rating average
 alert(`Thanks for your review! ${teacherName}'s average rating is now ${teacherAvgRating}.`)
@@ -40,18 +39,56 @@ alert(`Thanks for your review! ${teacherName}'s average rating is now ${teacherA
 //logs teacher info to console
 console.log(`Teacher: ${teacherName}\nDepartment: ${teacherDepartment}\nRatings: ${teacherRatingOutput}\nAverage Rating: ${teacherAvgRating}`);
 
+//variables containing student info
+const studentName = "Ida Dream";
+const studentMajor = "Communication";
+const studentEmail = "talkinfunny@gmail.com";
+const studentGPA = 3.4;
 
-let studentName = "Ida Dream";
-let studentMajor = "Communication";
-let studentEmail = "talkinfunny@gmail.com";
-let studentGPA = 3.4;
-
+//logs student info to console
 console.log(`Student Name: ${studentName}\nMajor: ${studentMajor}\nEmail: ${studentEmail}\nGPA: ${studentGPA}`);
 
+//variables containing course info
+const courseName = "Interpersonal Communication";
+const courseDepartment = "Communication";
+const courseTeacher = "Thomas Talks-a-lot";
+const courseSemester = "Fall 2019";
 
-let courseName = "Interpersonal Communication";
-let courseDepartment = "Communication";
-let courseTeacher = "Thomas Talks-a-lot";
-let courseSemester = "Fall 2019";
+//array containing courses offered and their departments
+const courseCatalog = [
+    ["Interpersonal Communication", "Communication"],
+    ["Survival 101", "Random Electives"],
+    ["Calculus", "Mathematics"],
+    ["Business 101", "Business"],
+    ["Speech 101", "Communication"],
+    ["Dothraki", "Random Electives"],
+    ["Algebra I", "Mathematics"],
+    ["Business Finance", "Business"]
+]
 
+//filters courses by given department, returns only course names within given department
+const courseFilter = (array, department) => {
+    let departmentCourses = [];
+    let newArray = array.filter(x => x[1] === department);
+    newArray.forEach(x => departmentCourses.push(x[0]));
+    return departmentCourses;
+}
+
+//checks whether given department exists, saves filtered courses
+let filterInput;
+let departmentCourses;
+
+while (true) {
+    filterInput = window.prompt("Please enter a department name to view courses.");
+    departmentCourses = courseFilter(courseCatalog, filterInput);
+    if (departmentCourses && departmentCourses.length) {        
+        break;
+    }
+    alert("The department you entered doesn't exist. Please enter a valid department name.");
+}
+
+//tells student courses available in input department
+alert(`The following courses are available in the ${filterInput} department: ${departmentCourses.join(", ")}`);
+
+//logs course info to console
 console.log(`Course: ${courseName}\nDepartment: ${courseDepartment}\nInstructor: ${courseTeacher}\nSemester: ${courseSemester}`);
